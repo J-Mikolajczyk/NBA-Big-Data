1c5428ded3ad5e004c488241be1f0a6ad9c3931a
Sai Donepudi <dlastpeeps@gmail.com>
2024-11-10 12:02:55 -0700

    add some code to read the play-by-play data

--- src/main/java/org/csu/cs435/NBABigData.java
+++ src/main/java/org/csu/cs435/NBABigData.java
@@ -16,9 +16,28 @@ import java.util.Map;
 public class NBABigData {
 
     public static void main(String[] args) {
-        if(args.length != 1) {
+        if (args.length != 1) {
             System.err.println("Usage: NBABigData <play-by-play.csv>");
         }
-        // todo add implementation
+        SparkSession spark = createSparkSession();
+
+        Dataset<String> rawNbaPlayByPlay = readData(spark, args[0]);
+
+        System.out.println("First few rows of the Dataset:");
+        nbaPlayByPlay.show(5);
+    }
+
+    private static SparkSession createSparkSession() {
+        return SparkSession.builder()
+                .appName("NBA Play-by-Play Data Processing")
+                .master("local")
+                .getOrCreate();
+    }
+
+    private static Dataset<String> readData(final SparkSession spark, final String filePath) {
+        return spark.read()
+                .option("header", "true")
+                .option("inferSchema", "true")
+                .csv(filePath);
     }
 }
